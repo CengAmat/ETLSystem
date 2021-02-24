4eab6094791e3a4c498ef72cd3f791336a3ae393
Ahmet Zurnaci <cengamat@gmail.com>
2021-02-24 15:43:03 +0000

    first commit

---
+++ ETLSystem/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ETLSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
